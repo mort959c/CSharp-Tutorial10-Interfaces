dd3ee6cd3cd724c3dfb92a1dc4826e81c47c17e5
Morten <morten.graabek@gmail.com>
2018-02-04 17:28:20 +0100

    s
    d

---
+++ 10 - Interfaces/App/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle buick = new Vehicle("Buick", 4, 160);
+
+            if(buick is IDriveable)
+            {
+                buick.Move();
+                buick.Stop();
+            }
+            else
+            {
+                Console.WriteLine("The {0} can't be driven", buick.Brand);
+            }
+
+
+            #region TV
+            IElectronicDevice TV = TVRemote.GetDevice();
+
+            PowerButton powerButton = new PowerButton(TV);
+
+            powerButton.Execute();
+            powerButton.Undo();
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
